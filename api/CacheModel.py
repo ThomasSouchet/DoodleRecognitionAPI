1bf2dd4a048d9c5545002b5c0bc3521151776154
Thomas Souchet <thomas.souchet@gmail.com>
2021-03-12 16:20:06 +0100

    Implementation of the predict service

---
+++ api/CacheModel.py
@@ -0,0 +1,26 @@
+import os
+
+class  CacheModel:
+    
+    __instance = None
+    model = None
+    
+    @staticmethod 
+    def getInstance():
+        """ Static access method. """
+        if CacheModel.__instance == None:
+            CacheModel()
+        return CacheModel.__instance
+    
+    def __init__(self):
+        """ Virtually private constructor. """
+        if CacheModel.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            CacheModel.__instance = self
+    
+    def getModel(self):
+        return self.model
+    
+    def setModel(self, model):
+        self.model = model
--- api/fast.py
+++ api/fast.py
@@ -1,9 +1,17 @@
 
 import pandas as pd
+import numpy as np
 import joblib
-
-from fastapi import FastAPI
+import os
+import time
+import shutil
+import json
+from api.CacheModel import CacheModel
+from PIL import Image
+from pylab import array
+from fastapi import FastAPI, Response, Request, Header, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from tensorflow.keras.models import load_model
 
 app = FastAPI()
 
@@ -15,27 +23,76 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
+def read_imagefile(file) -> Image.Image :
+    img = Image.open(file)
+    img = img.convert('1') # convert image to black and white
+    img = img.resize((28,28), Image.ANTIALIAS)
+    img = np.expand_dims(array(img), axis=-1)
+    img = img / 255
+    img = np.expand_dims(array(img), axis=0)
+    return array(img)
+
+@app.on_event("startup")
+async def startup_event():
+    '''
+    On api startup, load and store models in mem  
+    '''
+    print("load model ...")
+    dirname = os.path.dirname(os.path.dirname(__file__))
+    model_path = os.path.join(dirname,'models','my_model1')
+    model = load_model(model_path)
+    CacheModel.getInstance().setModel(model)
+    print("model is ready ...")
+
 @app.get("/")
 def index():
     return {"Doodle Recognition API": "OK"}
 
 
-@app.get("/predict/")
-def predict():
+@app.post("/predict/")
+def predict(response : Response, inputImage : UploadFile = File(...)):
 
-    # build X ⚠️ beware to the order of the parameters ⚠️
-    # X = pd.DataFrame(dict())
+    ''' 
+    Temp image
+    '''
+    temp_image = f'{str(int(time.time()))}-{inputImage.filename}'
+    with open(temp_image, "wb") as buffer:
+        shutil.copyfileobj(inputImage.file, buffer)
+ 
+    '''
+    Prediction worker
+    '''
+    # Extraction image
+    img = read_imagefile(temp_image)
 
-    # ⚠️ TODO: get model from GCP
+    # prediction
+    pred = CacheModel.getInstance().getModel().predict(img)
 
-    # pipeline = get_model_from_gcp()
-    # pipeline = joblib.load('model.joblib')
+    '''
+    Delete temp image
+    '''
+    if os.path.exists(temp_image):
+        os.remove(temp_image)
 
-    # make prediction
-    # results = pipeline.predict(X)
+    '''
+    Build response
+    '''
+    classes = []
+    with open('./api/params.json', 'rb') as f:
+        params = json.load(f)
+        nb_classes = params['number_of_classes']
+        classes = params['classes'][:nb_classes]
 
-    # convert response from numpy to python type
-    # pred = float(results[0])
+    predict10 = (pred[0] > 0.1)
 
-    # return dict(prediction=pred)
-    return {"Prediction": "It's a cat !!!"}
+    prediction = {}
+    cpt = 0
+    for bool in predict10:
+        if bool:
+            prediction[classes[cpt]] = str(round(pred[0][cpt]*100, 2))
+        cpt += 1
+    
+    response_prediction = {"prediction" : prediction}
+    response.status_code = 200 
+    response.headers["Content-Type"] = "application/json"
+    return response_prediction
